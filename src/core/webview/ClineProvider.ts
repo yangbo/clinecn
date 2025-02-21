cef57164f5666ec57f5f3c4d0ffba25616c25d60
yangbo <bo.yang@telecwin.com>
2025-02-21 17:09:37 +0800

    修正‘高级设置’打开的配置是cline而非clinecn的问题

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -799,7 +799,7 @@ export class ClineProvider implements vscode.WebviewViewProvider {
 						const settingsFilter = message.text || ""
 						await vscode.commands.executeCommand(
 							"workbench.action.openSettings",
-							`@ext:cline ${settingsFilter}`.trim(), // trim whitespace if no settings filter
+							`@ext:clinecn ${settingsFilter}`.trim(), // trim whitespace if no settings filter
 						)
 						break
 					}
